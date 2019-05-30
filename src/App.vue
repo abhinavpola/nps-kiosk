584827e55c2afc640388a32a3cf3e3c0d97ba7d6
Abhinav Pola <pola2@illinois.edu>
2019-05-29 21:50:03 -0700

    implemented state filter

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
   <div id="app">
     <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
       <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/">{{title}}</a>
-      <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" v-model="searchQuery">
+      <input
+        class="form-control form-control-dark w-100"
+        type="text"
+        placeholder="Search"
+        aria-label="Search"
+        v-model="searchQuery"
+      >
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
           <a class="nav-link" href="#">Github</a>
@@ -17,113 +23,161 @@
             <ul class="nav flex-column">
               <li class="nav-item">
                 <a class="nav-link active" href="#">
-                  Filter #1 <span class="sr-only">(current)</span>
+                  State
+                  <span class="sr-only">(current)</span>
                 </a>
+                <select name="state" id="state" v-model="stateFilter">
+                  <option value="AL">Alabama</option>
+                  <option value="AK">Alaska</option>
+                  <option value="AZ">Arizona</option>
+                  <option value="AR">Arkansas</option>
+                  <option value="CA">California</option>
+                  <option value="CO">Colorado</option>
+                  <option value="CT">Connecticut</option>
+                  <option value="DE">Delaware</option>
+                  <option value="DC">District Of Columbia</option>
+                  <option value="FL">Florida</option>
+                  <option value="GA">Georgia</option>
+                  <option value="HI">Hawaii</option>
+                  <option value="ID">Idaho</option>
+                  <option value="IL">Illinois</option>
+                  <option value="IN">Indiana</option>
+                  <option value="IA">Iowa</option>
+                  <option value="KS">Kansas</option>
+                  <option value="KY">Kentucky</option>
+                  <option value="LA">Louisiana</option>
+                  <option value="ME">Maine</option>
+                  <option value="MD">Maryland</option>
+                  <option value="MA">Massachusetts</option>
+                  <option value="MI">Michigan</option>
+                  <option value="MN">Minnesota</option>
+                  <option value="MS">Mississippi</option>
+                  <option value="MO">Missouri</option>
+                  <option value="MT">Montana</option>
+                  <option value="NE">Nebraska</option>
+                  <option value="NV">Nevada</option>
+                  <option value="NH">New Hampshire</option>
+                  <option value="NJ">New Jersey</option>
+                  <option value="NM">New Mexico</option>
+                  <option value="NY">New York</option>
+                  <option value="NC">North Carolina</option>
+                  <option value="ND">North Dakota</option>
+                  <option value="OH">Ohio</option>
+                  <option value="OK">Oklahoma</option>
+                  <option value="OR">Oregon</option>
+                  <option value="PA">Pennsylvania</option>
+                  <option value="RI">Rhode Island</option>
+                  <option value="SC">South Carolina</option>
+                  <option value="SD">South Dakota</option>
+                  <option value="TN">Tennessee</option>
+                  <option value="TX">Texas</option>
+                  <option value="UT">Utah</option>
+                  <option value="VT">Vermont</option>
+                  <option value="VA">Virginia</option>
+                  <option value="WA">Washington</option>
+                  <option value="WV">West Virginia</option>
+                  <option value="WI">Wisconsin</option>
+                  <option value="WY">Wyoming</option>
+                </select>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Filter #2
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Filter #3
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Filter #4
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Filter #5
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Filter #6
-                </a>
+                <a class="nav-link" href="#">Designation</a>
               </li>
             </ul>
           </div>
         </nav>
 
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <div
+            class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"
+          >
             <h1 class="h2">Results</h1>
             <div class="btn-toolbar mb-2 mb-md-0">
               <div class="btn-group mr-2">
                 <button class="btn btn-sm btn-outline-secondary">Filter</button>
                 <button class="btn btn-sm btn-outline-secondary">Filter</button>
               </div>
-              <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                Sort
-              </button>
+              <button class="btn btn-sm btn-outline-secondary dropdown-toggle">Sort</button>
             </div>
           </div>
           <div id="accordion">
             <div v-for="park in filteredResources" class="card">
-                <div class="card-header">
-                  <h5 class="mb-0">
-                    <button class="btn btn-link" data-toggle="collapse" v-bind:data-target="'#'+park['parkCode']" aria-expanded="false">
-                      {{park["fullName"]}}
-                    </button>
-                  </h5>
-                </div>
+              <div class="card-header">
+                <h5 class="mb-0">
+                  <button
+                    class="btn btn-link"
+                    data-toggle="collapse"
+                    v-bind:data-target="'#'+park['parkCode']"
+                    aria-expanded="false"
+                  >{{park["fullName"]}}</button>
+                </h5>
+              </div>
 
-                <div v-bind:id="park['parkCode']" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-                  <div class="card-body">
-                    {{park["description"]}}
+              <div
+                v-bind:id="park['parkCode']"
+                class="collapse"
+                aria-labelledby="headingOne"
+                data-parent="#accordion"
+              >
+                <div class="card-body">
+                  {{park["description"]}}
+                  <div class="row">
+                    <div class="col-2"></div>
                   </div>
                 </div>
               </div>
+            </div>
           </div>
         </main>
       </div>
     </div>
-
-    
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
       parks: [],
-      title: 'National Park Services',
+      title: "National Park Services",
       searchQuery: "",
+      stateFilter: "",
       filteredResources: []
-    }
+    };
   },
   mounted() {
     axios
-      .get('https://developer.nps.gov/api/v1/parks?limit=5&api_key=8IM8T7wUtRMc8yiwfTaaWeTXMDJeEXhmZWDdmJ1b')
+      .get(
+        "https://developer.nps.gov/api/v1/parks?limit=15&api_key=8IM8T7wUtRMc8yiwfTaaWeTXMDJeEXhmZWDdmJ1b"
+      )
       .then(response => {
-        this.parks = response["data"]["data"]
-        this.filteredResources = this.parks
+        this.parks = response["data"]["data"];
+        this.filteredResources = this.parks;
       })
       .catch(error => {
-        console.log(error)
-      })
+        console.log(error);
+      });
   },
   watch: {
     searchQuery() {
       this.filteredResources = this.parks.filter(park =>
         park["fullName"].toUpperCase().includes(this.searchQuery.toUpperCase())
       );
+    },
+    stateFilter() {
+      this.filteredResources = this.parks.filter(park => 
+        park["states"] === (this.stateFilter)
+      )
     }
   }
-
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
